9854876951cd51f9b8afac2b430693b4eb15e0b7
Stanislav <stastetin@gmail.com>
2018-07-19 17:29:00 +0300

    Initial dev branch

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,8 @@ module.exports = {
     },
     devtool : 'eval',
     plugins: [
-        new HtmlWebpackPlugin({ // плагин для  генерацияя шаблона в /build/
+        // плагин для  генерацияя шаблона в /build/
+        new HtmlWebpackPlugin({
             title: 'Slides',
             minify: {
                 collapseWhitespace: false
@@ -24,7 +25,8 @@ module.exports = {
             hash: true,
             template: './static/index.html'
         }),
-        new MiniCssExtractPlugin({ // плагин для генерации отдельного .css файла в /build/
+        // плагин для генерации отдельного .css файла в /build/
+        new MiniCssExtractPlugin({
             filename: 'bundle.css'
         })
     ],
@@ -73,5 +75,4 @@ module.exports = {
             }
         ]
     },
-
 };
